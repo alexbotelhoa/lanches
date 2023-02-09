8d0d13b3bbbed7982ef87818ac758b394da9e40e
alexbotelhoa <alexbotelho1@hotmail.com>
2023-02-09 17:14:38 -0300

    Add Controler Snack

---
+++ Controllers/SnackController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using LanchesBotelho.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LanchesBotelho.Controllers
+{
+    public class SnackController : Controller
+    {
+        private readonly ILogger<SnackController> _logger;
+        private readonly ISnackRepository _snackRepository;
+
+        public SnackController(
+            ILogger<SnackController> logger,
+            ISnackRepository snackRepository)
+        {
+            _logger = logger;
+            _snackRepository = snackRepository;
+        }
+
+        public IActionResult List()
+        {
+            var snack = _snackRepository.Snacks;
+            return View(snack);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
